4d2812846c75b66a16e260e25b5cad3efb006a86
Bram <bram@kabisa.nl>
2011-09-23 14:22:59 +0200

    crap

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+  def add(str = "")
+    str.split(/(\,|\n)/).map(&:to_i).reduce(:+) || 0
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,44 @@
 require 'string_calculator'
 
 describe StringCalculator do
+  it "should return 0 if an nothing is given" do
+    subject.add().should == 0
+  end
+  
+  it "should return 0 if 0 is given" do
+    subject.add("0").should == 0    
+  end  
+  
+  it "should return 0 if 2 0's are given" do
+    subject.add("0, 0").should == 0        
+  end  
+  
+  it "should return 1 if 1 is given" do
+    subject.add("1").should == 1
+  end
+    
+  it "should return 1 if 0, 1 is given" do
+    subject.add("0, 1").should == 1
+  end  
+  
+  it "should return 1 if 1, 0 is given" do
+    subject.add("1, 0").should == 1
+  end  
+  
+  it "should return 4 if 2, 2 is given" do
+    subject.add("2, 2").should == 4
+  end  
+  
+  it "should be able to handle over 9000 numbers" do
+    arr = 9999.times.collect
+    subject.add(arr.join(", ")).should == arr.reduce(:+)
+  end
+  
+  it "should be able to handle newlines and comma's" do
+    subject.add("2\n2").should == 4
+  end
+  
+  it "should be able to handle newlines and comma's" do
+    subject.add("1\n2,3").should == 6
+  end  
 end
